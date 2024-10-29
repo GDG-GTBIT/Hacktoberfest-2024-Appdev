fdedd88a5244f181d262d63cae494d574e3cf9ec
Pratyush Singh <pratyushsingh2005@gmail.com>
2024-10-30 00:30:47 +0530

    Added Settings to change temperature and units

--- flutter/weather_app/lib/screens/settings_page.dart
+++ flutter/weather_app/lib/screens/settings_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:weather_app/main.dart';
-import 'package:weather_app/screens/homescreen.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -12,87 +10,133 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  bool isDarkMode = false;
+  int _selectedIndex = 0;
 
-  void toggleTheme(bool value) {
+  void _onItemTapped(int index) {
     setState(() {
-      isDarkMode = value;
-      if (isDarkMode) {
-        // Change theme to dark
-        MyApp.of(context).changeTheme(ThemeMode.dark);
-      } else {
-        // Change theme to light
-        MyApp.of(context).changeTheme(ThemeMode.light);
-      }
+      _selectedIndex = index;
     });
+    if (index == 0) {
+      context.go('/');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _selectedIndex == 0 ? buildScaffold() : Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildScaffold() {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
+        foregroundColor: Theme.of(context).brightness == Brightness.light
+            ? Colors.black
+            : Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
       ),
-      body: Center(
-        child: SwitchListTile(
-          title: const Text('Dark Mode'),
-          value: isDarkMode,
-          onChanged: toggleTheme,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("lib/assets/bg.png"), // Path to your image
+            fit: BoxFit.cover, // How the image should be fitted
+          ),
+        ),
+        child: ListView(
+          children: [
+            ListTile(
+              title: const Text('Enable Hourly Weather Alerts'),
+              trailing: Switch(
+                value: true,
+                onChanged: (value) {},
+                activeColor: const Color.fromARGB(
+                    255, 203, 201, 201), // Change the active color
+                activeTrackColor: const Color.fromARGB(255, 203, 201, 201)
+                    .withOpacity(0.7), // Change the active track color
+                inactiveThumbColor:
+                    Colors.grey, // Change the inactive thumb color
+                inactiveTrackColor: Colors.grey
+                    .withOpacity(0.7), // Change the inactive track color
+              ),
+            ),
+            ListTile(
+              title: const Text('Temperature'),
+              subtitle: DropdownButton<String>(
+                value: 'Celsius',
+                items: <String>['Celsius', 'Fahrenheit']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {});
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Wind Speed'),
+              subtitle: DropdownButton<String>(
+                value: 'Miles per hour',
+                items: <String>['Kilometers per hour', 'Miles per hour']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {});
+                },
+              ),
+            ),
+            ListTile(
+              title: const Text('Pressure'),
+              subtitle: DropdownButton<String>(
+                value: 'Millibars',
+                items: <String>['Millibars', 'Inches of mercury']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {});
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              padding: const EdgeInsets.all(16),
+              child: const Text(
+                'Powered by OpenWeatherMap API',
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-class MyApp extends InheritedWidget {
-  MyApp({
-    super.key,
-    required super.child,
-  });
-
-  // ignore: library_private_types_in_public_api
-  static _MyAppState of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<MyApp>()!._data;
-  }
-
-  final _MyAppState _data = _MyAppState();
-
-  @override
-  bool updateShouldNotify(MyApp oldWidget) => true;
-}
-
-class _MyAppState extends State<SettingsPage> {
-  ThemeMode _themeMode = ThemeMode.system;
-
-  void changeTheme(ThemeMode themeMode) {
-    setState(() {
-      _themeMode = themeMode;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Weather App',
-      theme: ThemeData.light(), // Define light theme
-      darkTheme: ThemeData.dark(), // Define dark theme
-      themeMode: _themeMode, // Use themeMode from state
-      routerConfig: GoRouter(routes: [
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const Homescreen(),
-        ),
-        GoRoute(
-          path: '/settings',
-          builder: (context, state) => const SettingsPage(),
-        ),
-      ]),
-    );
-  }
-}
-
-void main() {
-  runApp(MyApp(
-    child: const WeatherApp(),
-  ));
 }
